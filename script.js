d63ef0bdf36401b7476dbf5769cd48c36044062a
Polochtron <paul.cheveau88@gmail.com>
2022-04-09 11:28:37 +0200

    test

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 var nbAleatoires=Math.floor(Math.random()*1000)+1;
 var solution=document.getElementById("solution");
-console.log("test");
+var rep=document.getElementById("rep");
 
-function compare(rep) 
+function compare() 
 {
     if (rep.value==nbAleatoires)
     {
-        console.log("rv");
         solution.innerHTML="Bravo! Vous avez trouvez le nombre";
     }
     else if (rep.value>nbAleatoires)
     {
-        console.log("rv");
         solution.innerHTML="C'est plus petit !";
     }
     else if (rep.value>nbAleatoires)
     {
-        console.log("rv");
         solution.innerHTML="C'est plus grand !";
     }
 }
