5575b39eafc5738e9e21cfc03af6f04ef2035842
Polochtron <paul.cheveau88@gmail.com>
2022-04-09 11:11:11 +0200

    test2

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function compare(this) 
+var nbAleatoires=Math.floor(Math.random()*1000)+1;
+var solution=document.getElementById("solution");
+function compare(rep) 
 {
+    if (rep==nbAleatoires)
+    {
+        solution.innerHTML="Bravo! Vous avez trouvez le nombre"
+    }
+    else if (rep>nbAleatoires)
+    {
+        solution.innerHTML="C'est plus petit !"
+    }
+    else if (rep>nbAleatoires)
+    {
+        solution.innerHTML="C'est plus grand !"
+    }
     var a=1
 }
